88bd24b61a4d97128e6f7605e2b6d3d48dc074e3
Jose Torreblanca <josewhitetower@gmail.com>
2020-12-22 16:30:05 +0100

    styled logs table

--- components/LogsTable.js
+++ components/LogsTable.js
@@ -1,21 +1,21 @@
 export default function LogsTable({user}) {
   return (
     <table className="w-full mt-4">
-      <caption className="mb-2 font-semibold">{user.username}'s Logs</caption>
+      <caption className="mb-2 font-semibold text-white mb-6">{user.username}'s Logs</caption>
       <thead>
-        <tr className="bg-gray-200 border border-gray-300">
-          <th>Description</th>
-          <th>Duration</th>
-          <th>Date</th>
+        <tr className="bg-green-500 border text-white border-gray-300">
+          <th className="p-1">Description</th>
+          <th className="p-1">Duration</th>
+          <th className="p-1">Date</th>
         </tr>
       </thead>
       <tbody>
         {user.log && user.log.map((log) => {
           return (
-            <tr className="border-gray-300 border" key={log.date}>
-              <td className="border-gray-300 border text-right">{log.description}</td>
-              <td className="border-gray-300 border text-right">{log.duration}</td>
-              <td className="border-gray-300 border text-right">{new Date(log.date).toDateString()}</td>
+            <tr className="border-gray-300 border text-white" key={log.date}>
+              <td className="border-gray-300 border text-right p-1">{log.description}</td>
+              <td className="border-gray-300 border text-right p-1">{log.duration}</td>
+              <td className="border-gray-300 border text-right p-1">{new Date(log.date).toDateString()}</td>
             </tr>
           );
         })}
