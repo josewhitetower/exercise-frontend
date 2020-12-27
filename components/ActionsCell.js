392311a98ed53a2eff7a9313365448800bb41d41
Jose Torreblanca <josewhitetower@gmail.com>
2020-12-27 16:33:41 +0100

    added cursor pointer class

--- components/ActionsCell.js
+++ components/ActionsCell.js
@@ -12,14 +12,14 @@ export default function ActionsCell({onEdit, log, isEditing}) {
     <td className="border-gray-300 border p-1">
       <div className="flex place-content-center">
         <div
-          className="mr-2 focus:outline-none focus:ring-2 focus:ring-green-400"
+          className="mr-2 focus:outline-none focus:ring-2 focus:ring-green-400 cursor-pointer"
           onClick={handleClick}
           onKeyDown={handleKeyDown}
           tabIndex={0}
         >
           {isEditing ? '💾' : '📝'}
         </div>
-        <div tabIndex={0} className="mr-2 focus:outline-none focus:ring-2 focus:ring-green-400" >⛔</div>
+        <div tabIndex={0} className="mr-2 focus:outline-none focus:ring-2 focus:ring-green-400 cursor-pointer">⛔</div>
       </div>
     </td>
   );
